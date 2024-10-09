ef3ce4aa449cfc732bf3181372cec78dc45c257f
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-10-09 12:02:57 +0200

    e2_uitnodigingen done

---
+++ e2_uitnodigingen.mjs
@@ -0,0 +1,5 @@
+let students = ['John', 'Steven', 'Liam', 'Wouter', 'Luke', 'Kirk'];
+
+for(let i in students) {
+    console.log(`${students[i]}, je bent uitgenodigd om deel te nemen aan de leukste cursus van EhB: Programming Essentials`);
+}
