cbd3849e2f73c1924236669cdd0681dd1bc13013
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-10-09 12:06:16 +0200

    Better Javascript practice

--- e2_uitnodigingen.mjs
+++ e2_uitnodigingen.mjs
@@ -1,5 +1,5 @@
 let students = ['John', 'Steven', 'Liam', 'Wouter', 'Luke', 'Kirk'];
 
-for(let i in students) {
+for(let i = 0; i < students.length; i++) {
     console.log(`${students[i]}, je bent uitgenodigd om deel te nemen aan de leukste cursus van EhB: Programming Essentials`);
 }
